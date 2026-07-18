302ba96df40dbebb9b51f6824b1b412b54482ee5
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-07-18 11:04:14 -0500

    listo

--- app/Models/Taxonomy.php
+++ app/Models/Taxonomy.php
@@ -11,4 +11,17 @@ class Taxonomy extends Model
     {
         return $this->hasMany(Project::class);
     }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($taxonomy) {
+
+            if ($taxonomy->projects()->exists()) {
+                throw new \Exception('No puede eliminar esta categoría porque tiene proyectos asociados.');
+            }
+
+        });
+    }
 }
