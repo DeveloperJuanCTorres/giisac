ccfd7578bc81853bdcf6605e13a46b9eae9b36e8
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-07-28 21:10:25 -0500

    ca

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,7 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     public function taxonomy()
-{
-    return $this->belongsTo(Taxonomy::class, 'taxonomy_id');
-}
+    {
+        return $this->belongsTo(Taxonomy::class, 'taxonomy_id');
+    }
+
+    protected static function booted()
+    {
+        static::creating(function ($product) {
+
+            if (!request()->hasFile('imagen')) {
+
+                throw \Illuminate\Validation\ValidationException::withMessages([
+                    'imagen' => 'La imagen es obligatoria.'
+                ]);
+
+            }
+
+        });
+    }
 }
